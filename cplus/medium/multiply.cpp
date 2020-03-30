ea1641b0501c163a54af6007790585d7a5ef60bf
iwzy7071 <iwzy7071@gmail.com>
2020-03-30 10:55:31 +0800

    Finish multiply.cpp

---
+++ cplus/medium/multiply.cpp
@@ -0,0 +1,28 @@
+//
+// Created by WangZhongyu on 2020/3/30.
+//
+
+class Solution{
+        public:
+        string multiply(string num1, string num2) {
+            int n1 = num1.size();
+            int n2 = num2.size();
+            string
+            res(n1 + n2, '0');
+            for (int i = n2 - 1; i >= 0; i--) {
+                for (int j = n1 - 1; j >= 0; j--) {
+                    int temp = (res[i + j + 1] - '0') + (num1[j] - '0') * (num2[i] - '0');
+                    res[i + j + 1] = temp % 10 + '0';
+                    res[i + j] += temp / 10;
+                }
+            }
+
+            for (int i = 0; i < n1 + n2; i++) {
+                if (res[i] != '0')
+                    return res.substr(i);
+            }
+            return "0";
+
+
+        }
+};
