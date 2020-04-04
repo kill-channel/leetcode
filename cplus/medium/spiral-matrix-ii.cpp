c7ea3693e356adc5014ac05e02e3a113b99eb084
iwzy7071 <iwzy7071@gmail.com>
2020-04-04 10:19:41 +0800

    Finish merge-intervals.cpp and spiral-matrix-ii.cpp

--- cplus/medium/spiral-matrix-ii.cpp
+++ cplus/medium/spiral-matrix-ii.cpp
@@ -8,26 +8,26 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         int left = 0, right = n - 1, up = 0, low = n - 1;
         int current = 1;
-        vector<vector<int>> ans;
+        vector<vector<int>> ans(n, vector<int>(n));;
         while (true) {
             for (int i = left; i <= right; i++)
-                ans[i][up] = current++;
-            if (++up >= low)
+                ans[up][i] = current++;
+            if (++up > low)
                 return ans;
 
             for (int i = up; i <= low; i++)
-                ans[right][i] = current++;
-            if (--right <= left)
+                ans[i][right] = current++;
+            if (--right < left)
                 return ans;
 
             for (int i = right; i >= left; i--)
-                ans[i][low] = current++;
-            if (--low <= up)
+                ans[low][i] = current++;
+            if (--low < up)
                 return ans;
 
             for (int i = low; i >= up; i--)
                 ans[i][left] = current++;
-            if (++left >= right)
+            if (++left > right)
                 return ans;
         }
     }
